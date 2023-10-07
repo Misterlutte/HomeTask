535bc65052ab681a9c68c102f83c8d7d2486b0bf
EbatKopat <nzagnetov@bk.ru>
2023-10-07 15:57:31 +0300

    added a tasks and solutions

--- Program.cs
+++ Program.cs
@@ -1,94 +1,82 @@
-﻿// Задача 2: Напишите программу, которая на вход принимает два числа и выдаёт, какое число большее, а какое меньшее.
+﻿// Задача 19: Напишите программу, которая принимает на вход пятизначное число и проверяет, является ли оно палиндромом.
+// 14212 -> нет
+// 23432 -> да
+// 12821 -> да
 
-
-int max = 0;
-int min = 0;
-
-Console.Write("Введите первое число: ");
-int firstNumber = Convert.ToInt32(Console.ReadLine());
-
-Console.Write("Введите второе число: ");
-int secondNumber = Convert.ToInt32(Console.ReadLine());
-
-if(firstNumber > secondNumber)
-{
-    max = firstNumber;
-    min = secondNumber;
-}
-else
-{
-    max = secondNumber;
-    min = firstNumber;
-}
-Console.WriteLine("max = " + max + " и " + "min = " + min);
-
-
-// Задача 4: Напишите программу, которая принимает на вход три числа и выдаёт максимальное из этих чисел. (Сравнить с каждым)
-
-
-int max = 0;
-
-Console.Write("Введите первое число: ");
-int firstNumber = Convert.ToInt32(Console.ReadLine());
-
-Console.Write("Введите второе число: ");
-int secondNumber = Convert.ToInt32(Console.ReadLine());
-
-Console.Write("Введите третье число: ");
-int thirdNumber = Convert.ToInt32(Console.ReadLine());
-
-if(firstNumber > max)
-{
-    max = firstNumber;
-}
-if(secondNumber > max)
-{
-    max = secondNumber;
-}
-if(thirdNumber > max)
-{
-    max = thirdNumber;
-}
-
-Console.WriteLine("max = " + max);
-
-
-
-// Задача 6: Напишите программу, которая на вход принимает число и выдаёт, является ли число чётным (делится ли оно на два без остатка).
-
-
+Console.WriteLine("Задача 19");
 Console.Write("Введите число: ");
-int number = Convert.ToInt32(Console.ReadLine());
+string? number = Console.ReadLine();
 
-int remDiv = number % 2;
-
-if(remDiv == 0)
-{
-    Console.WriteLine("Да");
-}
-else
-{
-    Console.WriteLine("Нет");
+void CheckingNumber(string number){
+  if (number[0]==number[4] || number[1]==number[3]){
+    Console.WriteLine($"Ваше число: {number} - палиндром.");
+  }
+  else Console.WriteLine($"Ваше число: {number} - НЕ палиндром.");
 }
 
-
-
-
-
-// Задача 8: Напишите программу, которая на вход принимает число (N), а на выходе показывает все чётные числа от 1 до N.
-
-
-
-Console.Write("Введите число: ");
-int number = Convert.ToInt32(Console.ReadLine());
-
-int evenNumber = 2;
-
-if(number > 1)
-{
-    while(evenNumber <= number)
-    {
-        Console.Write(evenNumber + " ");
-        evenNumber = evenNumber + 2;
-    }
+if (number!.Length == 5){
+  CheckingNumber(number);
 }
+else Console.WriteLine($"Введи правильное число");
+
+
+ // Задача 21: Напишите программу, которая принимает на вход координаты двух точек и находит расстояние между ними в 3D пространстве.
+ // A (3,6,8); B (2,1,-7), -> 15.84
+ // A (7,-5, 0); B (1,-1,9) -> 11.53
+
+ Console.WriteLine("\n Задача 21");
+ int x1 = Coordinate("x", "A");
+ int y1 = Coordinate("y", "A");
+ int z1 = Coordinate("z", "A");
+ int x2 = Coordinate("x", "B");
+ int y2 = Coordinate("y", "B");
+ int z2 = Coordinate("z", "B");
+
+ int Coordinate(string coorName, string pointName)
+ {
+     Console.Write($"Введите координату {coorName} точки {pointName}: ");
+     return Convert.ToInt16(Console.ReadLine());
+ }
+
+ double Decision(double x1, double x2, 
+                 double y1, double y2, 
+                 double z1, double z2){
+   return Math.Sqrt(Math.Pow((x2-x1), 2) + 
+                    Math.Pow((y2-y1), 2) + 
+                    Math.Pow((z2-z1), 2));
+ }
+
+ double segmentLength =  Math.Round (Decision(x1, x2, y1, y2, z1, z2), 2 );
+
+ Console.WriteLine($"Длина отрезка  {segmentLength}");
+
+
+ //Задача 23: Напишите программу, которая принимает на вход число (N) и выдаёт таблицу кубов чисел от 1 до N.
+  // 3 -> 1, 8, 27
+ // 5 -> 1, 8, 27, 64, 125
+
+ Console.WriteLine("\n Задача 23");
+ Console.Write("Введите число: ");
+ int cube = Convert.ToInt32(Console.ReadLine());
+
+ void Cube(int[] cube){
+   int counter = 0;
+   int length = cube.Length;
+    while (counter <  length){
+    cube[counter] = Convert.ToInt32(Math.Pow(counter, 3));
+     counter++;
+   }
+ }
+
+ void PrintArry(int[] coll){
+   int count = coll.Length;
+   int index = 0;
+   while(index < count){
+     Console.Write(coll[index]+ " ");
+     index++;
+   }
+ } 
+
+ int[] arry = new int[cube+1];
+ Cube(arry);
+ PrintArry(arry);
