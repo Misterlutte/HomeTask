dcc13d6160adb08fea09bd2ba6eae6032348ab8f
EbatKopat <nzagnetov@bk.ru>
2023-10-22 13:15:59 +0300

    added a tasks and solutions

--- Program.cs
+++ Program.cs
@@ -1,117 +1,131 @@
-﻿Напишите программу, которая подсчитывает количество четных элементов в массиве целых положительных трехзначных чисел и выводит результат на экран.
+﻿// Задайте двумерный массив размером m×n, заполненный случайными вещественными числами.
 
-Console.WriteLine("Введите размер массива");
-int size = Convert.ToInt32(Console.ReadLine());
-int[] numbers = new int[size];
-int count = 0;
+Console.WriteLine("введите количество строк");
+int linesVol = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("введите количество столбцов");
+int columnsVol = Convert.ToInt32(Console.ReadLine());
+double[,] numbers = new double[linesVol, columnsVol];
 FillArrayRandomNumbers(numbers);
 PrintArray(numbers);
 
-
-for (int i = 0; i < numbers.Length; i++)
+void FillArrayRandomNumbers(double[,] array)
 {
-    if (numbers[i] % 2 == 0)
-    count++;
-}
-
-
-Console.WriteLine($"количество чётных чисел в массиве -> {count} ");
-
-void FillArrayRandomNumbers(int[] array)
-{
-    for(int i = 0; i < array.Length; i++)
+    for (int i = 0; i < array.GetLength(0); i++)
     {
-        array[i] = new Random().Next(100,1000);
+        for (int j = 0; j < array.GetLength(1); j++)
+        {
+            array[i, j] = Convert.ToDouble(new Random().Next(-100, 100)) / 10;
+        }
     }
 }
-void PrintArray(int[] array)
+
+void PrintArray(double[,] array)
 {
-    for(int i = 0; i < array.Length; i++)
+    for (int i = 0; i < array.GetLength(0); i++)
     {
-        Console.Write(array[i] + " ");
+        Console.Write("[ ");
+        for (int j = 0; j < array.GetLength(1); j++)
+        {
+            Console.Write(array[i, j] + " ");
+        }
+        Console.Write("]");
+        Console.WriteLine("");
     }
-    Console.WriteLine();
 }
 
-Напишите программу, которая находит сумму элементов с нечетными индексами в одномерном массиве целых чисел и выводит результат на экран.
+// Внутри класса Answer напишите методы CreateIncreasingMatrix, PrintArray, FindNumberByPosition и PrintCheckIfError.
 
-
-int size = 4;
-int[] numbers = new int[size];
+Console.WriteLine("введите номер строки");
+int n = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("введите номер столбца");
+int m = Convert.ToInt32(Console.ReadLine());
+int [,] numbers = new int [10,10];
 FillArrayRandomNumbers(numbers);
+
+if (n > numbers.GetLength(0) || m > numbers.GetLength(1))
+{
+    Console.WriteLine("такого элемента нет");
+}
+else
+{
+    Console.WriteLine($"значение элемента {n} строки и {m} столбца равно {numbers[n-1,m-1]}");
+}
+
 PrintArray(numbers);
 
-int sumNumbersEvenIndex = 0;
-
-for (int i = 1; i < numbers.Length; i += 2)
+void FillArrayRandomNumbers(int[,] array)
 {
-    sumNumbersEvenIndex += numbers[i];
+    for (int i = 0; i < array.GetLength(0); i++)
+        {        
+            for (int j = 0; j < array.GetLength(1); j++)
+            {
+                array [i,j] = new Random().Next(-100, 100)/10;
+            }   
+        }
 }
-Console.Write(sumNumbersEvenIndex);
 
-
-
-void FillArrayRandomNumbers(int[] array)
+void PrintArray(int[,] array)
 {
-    for (int i = 0; i < array.Length; i++)
+    for (int i = 0; i < array.GetLength(0); i++)
     {
-        array[i] = new Random().Next(-100, 101);
+        Console.Write("[ ");
+        for (int j = 0; j < array.GetLength(1); j++)
+        {
+            Console.Write(array[i,j] + " ");
+        }   
+        Console.Write("]");
+        Console.WriteLine(""); 
     }
 }
 
-void PrintArray(int[] array)
-{
-    for (int i = 0; i < array.Length; i++)
-    {
-        Console.Write(array[i] + " ");
-    }
-    Console.WriteLine();
-}
+// Внутри класса Answer напишите методы CreateIncreasingMatrix, PrintArray, PrintListAvr и FindAverageInColumns.
 
+Console.WriteLine("введите количество строк");
+int n = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("введите количество столбцов");
+int m = Convert.ToInt32(Console.ReadLine());
 
-Напишите программу для работы с массивом вещественных чисел.
-
-
-int size = 10;
-int[] numbers = new int[size];
+int[,] numbers = new int[n, m];
 FillArrayRandomNumbers(numbers);
+
+
+for (int j = 0; j < numbers.GetLength(1); j++)
+{
+    double avarage = 0;
+    for (int i = 0; i < numbers.GetLength(0); i++)
+    {
+        avarage = (avarage + numbers[i, j]);
+    }
+    avarage = avarage / n;
+    Console.Write(avarage + "; ");
+}
+Console.WriteLine();
 PrintArray(numbers);
 
-int max = numbers[0];
-int min = numbers[0];
 
-for (int i = 0; i < numbers.Length; i++)
+
+void FillArrayRandomNumbers(int[,] array)
 {
-    if (numbers[i] > max)
+    for (int i = 0; i < array.GetLength(0); i++)
     {
-        max = numbers[i];
-    }
-    else if (numbers[i] < min)
-    {
-        min = numbers[i];
+        for (int j = 0; j < array.GetLength(1); j++)
+        {
+            array[i, j] = new Random().Next(0, 10);
+        }
     }
 }
 
-Console.WriteLine($"Минимальное число: {min}");
-Console.WriteLine($"Минимальное число: {max}");
-Console.WriteLine($"Разница между максимальным и минимальным числами: {max-min}");
-
-
-
-void FillArrayRandomNumbers(int[] array)
+void PrintArray(int[,] array)
 {
-    for (int i = 0; i < array.Length; i++)
-    {
-        array[i] = new Random().Next(1, 555);
-    }
-}
 
-
-void PrintArray(int[] array)
-{
-    for (int i = 0; i < array.Length; i++)
+    for (int i = 0; i < array.GetLength(0); i++)
     {
-        Console.Write(array[i] + " ");
+        Console.Write("[ ");
+        for (int j = 0; j < array.GetLength(1); j++)
+        {
+            Console.Write(array[i, j] + " ");
+        }
+        Console.Write("]");
+        Console.WriteLine("");
     }
-    Console.WriteLine();
 }
